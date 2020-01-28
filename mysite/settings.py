e5b784839abd76b16a7f445bcce9601b33f2078a
quang <nlq@dynadna.com>
2020-01-27 23:16:14 -0500

    deploy to Heroku and connect with Heroku postgres

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-#DATABASES = {
+#DATAABASES = {
 #    'default': {
 #        'ENGINE': 'django.db.backends.sqlite3',
 #        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
